1179f8bc79b93266f1af43bf67dddf06a3de9eff
ziyu <ziyuzhu398@gmail.com>
2025-04-28 11:09:25 -0400

    1

--- script.js
+++ script.js
@@ -5,34 +5,34 @@ var app = new Vue({
             {
                 ix: "01",
                 tclz: "t-empty",
-                key: "junifer resort",
-                loc: "Mammoth Lakes, Califonia",
+                key: "Blue Mountain",
+                loc: "Blue Mountain, Collingwood",
                 left: -25,
-                image: "https://images.unsplash.com/photo-1482867996988-29ec3a0f1aac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                image: "img/IMG_4424.jpg"
             },
             {
                 ix: "02",
                 tclz: "t-filled",
                 key: "tamback lodge",
-                loc: "Bear Valley, California",
+                loc: "Alpine Skiing Club, Collingwood",
                 left: 25,
-                image: "https://images.unsplash.com/photo-1507492147080-3d1b3e5cd0aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                image: "img/IMG_6240.webp"
             },
             {
                 ix: "03",
                 tclz: "t-empty",
                 key: "alphenhof lodge",
-                loc: "Bear Valley, California",
+                loc: "Alpine Skiing Club, Collingwood",
                 left: 75,
-                image: "https://images.unsplash.com/photo-1563444276-0ef016ff4ae5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
+                image: "img/IMG_6989.webp"
             },
             {
                 ix: "04",
                 tclz: "t-empty",
-                key: "china pick ski",
-                loc: "China Pick Mountain, California",
+                key: "OFSAA Qualifying",
+                loc: "Alpine Skiing Club, Collingwood",
                 left: 125,
-                image: "https://images.unsplash.com/photo-1504827274833-7db1774520e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                image: "img/IMG_5557.webp"
             }
         ]
     },
