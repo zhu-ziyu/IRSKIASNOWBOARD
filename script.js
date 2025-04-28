b957f59b57e2728bbd0f5004f2894bea611c2121
ziyu <ziyuzhu398@gmail.com>
2025-04-28 13:24:33 -0400

    1

--- script.js
+++ script.js
@@ -5,52 +5,55 @@ var app = new Vue({
             {
                 ix: "01",
                 tclz: "t-empty",
-                key: "Blue Mountain",
-                loc: "Blue Mountain, Collingwood",
-                left: -25,
-                image: "img/IMG_4424.jpg"
+                key: "OFSAA SKI Team GIRL 2024 - 2025",
+                loc: "Alpine Skiing Club, Collingwood",
+                left: -40,
+                image: "img/main3.png"
             },
             {
                 ix: "02",
                 tclz: "t-filled",
-                key: "tamback lodge",
+                key: "OFSAA SKI Team BOY 2024 - 2025",
                 loc: "Alpine Skiing Club, Collingwood",
-                left: 25,
-                image: "img/IMG_6240.webp"
+                left: 40,
+                image: "img/main4.png"
             },
             {
                 ix: "03",
                 tclz: "t-empty",
-                key: "alphenhof lodge",
+                key: "IRHS Ski & Snowboard Team 2024 - 2025",
                 loc: "Alpine Skiing Club, Collingwood",
-                left: 75,
-                image: "img/IMG_6989.webp"
+                left: 120,
+                image: "img/main1.png"
             },
             {
                 ix: "04",
                 tclz: "t-empty",
-                key: "OFSAA Qualifying",
+                key: "OFSAA SKI Team BOY 2024 - 2025",
                 loc: "Alpine Skiing Club, Collingwood",
-                left: 125,
-                image: "img/IMG_5557.webp"
+                left: 200,
+                image: "img/main2.png"
             }
         ]
     },
     methods: {
         next: function() {
+            // 每次向左移动 80%
             this.items = this.items.map((item, index) => ({
                 ...item,
-                left: item.left - 50,
+                left: item.left - 80,
                 tclz: index === 2 ? "t-filled" : "t-empty"
             }));
+            // 动画切换周期结束后，把第一个元素移到最右
             setTimeout(() => {
-                const [st, ...rest] = this.items;
-                this.items = [...rest, { ...st, left: 125 }];
+                const [first, ...rest] = this.items;
+                // 新加入的项，left 重置为 200（80*2.5）
+                this.items = [...rest, { ...first, left: 200 }];
+                // 等待短暂间隔后继续循环
                 setTimeout(() => this.next(), 500);
             }, 2200);
         }
     },
-
     created: function() {
         this.next();
     }
